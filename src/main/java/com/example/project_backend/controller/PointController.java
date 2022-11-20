377c32af86380c7ff5cfb3d7a5c5e5a062a70007
tranthaicong2610 <69552670+tranthaicong2610@users.noreply.github.com>
2022-11-20 10:32:14 +0700

    fix controller

--- src/main/java/com/example/project_backend/controller/PointController.java
+++ src/main/java/com/example/project_backend/controller/PointController.java
@@ -1,5 +1,6 @@
 package com.example.project_backend.controller;
 
+import com.example.project_backend.form.GenerationInformationByDistance;
 import com.example.project_backend.form.PointInfo;
 import com.example.project_backend.model.Point;
 import com.example.project_backend.service.IPointService;
@@ -34,26 +35,54 @@ public class PointController {
     }
 
     @PostMapping("/data")
-    public PointInfo pointClient(@RequestBody PointInfo pointInfo){
+    public List<GenerationInformationByDistance> pointClient(@RequestBody PointInfo pointInfo){
         //test
         List<String> gens1 = new ArrayList<>();
         List<String> gens2 = new ArrayList<>();
         List<String> gens3 = new ArrayList<>();
         List<String> gens4 = new ArrayList<>();
         List<String> gens5 = new ArrayList<>();
-
-        gens1.add("HANOI","HAIPHONG","HANAM","NAMDINH");
-        gens2.add("NAMDINH","HAIDUONG","NGHEAN","QUANGNAM","HANOI","HAIPHONG");
-        gens3.add("NAMDINH","HAIDUONG","NGHEAN","QUANGNAM","HANOI","HAIDUONG","NGHEAN","HAIPHONG");
-        gens4.add("NAMDINH","HAIDUONG","NGHEAN","THANHHOA","HAIDUONG","NGHEAN","QUANGNAM","HANOI","HAIPHONG");
-        gens4.add("HANOI","THANHHOA","HAIDUONG","NGHEAN","HAIPHONG","HANAM","NAMDINH");
+        gens1.add("HANOI");
+        gens1.add("HAIPHONG");
+        gens1.add("HANAM");
+        gens1.add("NAMDINH");
+        gens2.add("NAMDINH");
+        gens2.add("HAIDUONG");
+        gens2.add("NGHEAN");
+        gens2.add("QUANGNAM");
+        gens2.add("HANOI");
+        gens2.add("HAIPHONG");
+        gens3.add("NAMDINH");
+        gens3.add("HAIDUONG");
+        gens3.add("NGHEAN");
+        gens3.add("QUANGNAM");
+        gens3.add("HANOI");
+        gens3.add("HAIDUONG");
+        gens3.add("NGHEAN");
+        gens3.add("HAIPHONG");
+        gens4.add("NAMDINH");
+        gens4.add("HAIDUONG");
+        gens4.add("NGHEAN");
+        gens4.add("THANHHOA");
+        gens4.add("HAIDUONG");
+        gens4.add("NGHEAN");
+        gens4.add("QUANGNAM");
+        gens4.add("HANOI");
+        gens4.add("HAIPHONG");
+        gens4.add("HANOI");
+        gens4.add("THANHHOA");
+        gens4.add("HAIDUONG");
+        gens4.add("NGHEAN");
+        gens4.add("HAIPHONG");
+        gens4.add("HANAM");
+        gens4.add("NAMDINH");
         System.out.println(pointInfo.toString());
         List<GenerationInformationByDistance> list = new ArrayList<>();
-        list.add(123,gens1);
-        list.add(152,gens2);
-        list.add(185,gens3);
-        list.add(123,gens4);
-        list.add(225,gens5);
+        list.add(new GenerationInformationByDistance(123,  gens1));
+        list.add(new GenerationInformationByDistance(152,gens2));
+        list.add(new GenerationInformationByDistance(185,gens3));
+        list.add(new GenerationInformationByDistance(123,gens4));
+        list.add(new GenerationInformationByDistance(225,gens5));
         return list;
     }
 }
